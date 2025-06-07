58bba7a6f18d3e322df06a7780f74c3c999a9176
RJMM7 <00148625@uca.edu.sv>
2025-06-07 15:33:07 -0600

    Subo el segundo ejercicio: Ejercicio 2 - FOR

---
+++ Ejercicio 2 - FOR.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int alturaMaxima;
+    int alturaMinima = 1;
+
+    cout << "Por favor digite la altura que va a tener su piramide: ";
+    cin >> alturaMaxima;
+
+    for (int i = 1; i <= alturaMaxima; i++) {
+        cout << i << " ";
+        cout << alturaMaxima - 1 << endl;
+    }
+
+    return 0;
+}
+
+// quiero que se le reste a la altura maxima el numero anterior, seguido de un endl
